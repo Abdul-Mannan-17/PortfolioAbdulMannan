a539e344ce88714732ab3e7003a451640d3a5785
Abdul Mannan <80338307+Abdul-Mannan-17@users.noreply.github.com>
2024-07-01 14:28:16 +0500

    Update SkillSection.js
    comment cloud section

--- src/containers/skills/SkillSection.js
+++ src/containers/skills/SkillSection.js
@@ -13,9 +13,9 @@ function GetSkillSvg(props) {
     return <DataScienceImg theme={props.theme} />;
   else if (props.fileName === "FullStackImg")
     return <FullStackImg theme={props.theme} />;
-  else if (props.fileName === "CloudInfraImg")
-    return <CloudInfraImg theme={props.theme} />;
-  return <DesignImg theme={props.theme} />;
+ // else if (props.fileName === "CloudInfraImg")
+   // return <CloudInfraImg theme={props.theme} />;
+  //return <DesignImg theme={props.theme} />;
 }
 
 class SkillSection extends Component {
